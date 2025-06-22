f79f8748ef3529e448335b099c2e1df33fba5b23
Andres Asteasuain <andres.asteasuain@hotmail.com>
2025-06-22 18:00:33 -0300

    changes for Chapter 4

--- chapter4.py
+++ chapter4.py
@@ -1,6 +1,6 @@
 import random
 
-valid_choices = ['1', '2']
+valid_choices = ['1', '2', '3', '4']
 
 def show_random_numbers():
     for _ in range(10):
@@ -19,10 +19,59 @@ def definitions():
 
     repeat_lyrics()
 
+def compute_payment():
+    def compute_pay(hours, rate):
+        try:
+            hours = float(s_hours)
+            rate = float(s_rate)
+
+            if hours > 40:
+                excessHours = hours - 40
+                extraPayment = excessHours * (rate * 0.5)
+            else:
+                extraPayment = 0
+                
+            totalPay = hours * rate + extraPayment
+
+            print('The total pay is $' + str(round(totalPay, 2)))
+        except:
+            print('Error, please enter numeric input')
+
+    s_hours = input('Enter the number of hours worked: ')
+    s_rate = input('Enter the hourly rate: ')
+    compute_pay(s_hours, s_rate)
+
+def get_grade():
+    def compute_grade(score):
+        try:
+            score = float(s_score)
+            if score < 0 or score >= 1:
+                raise ValueError("Score must be between 0 and 10")
+            elif score >= 0.9:
+                grade = 'A'
+            elif score >= 0.8:
+                grade = 'B'
+            elif score >= 0.7:
+                grade = 'C'
+            elif score >= 0.6:
+                grade = 'D'
+            else:
+                grade = 'F'
+
+            print('Grade: ' + grade)
+        except:
+            print('Error, bad score')
+    
+    s_score = input('Enter the score: ')
+    compute_grade(s_score)
+
+        
 def get_choice():
     print("Functions: \n")
     print("1. ----->: show_random_numbers")
     print("2. ----->: definitions")
+    print("3. ----->: compute_payment")
+    print("4. ----->: get_grade")
     print("Other key: Exit")
     choice = input("Enter your choice: ")
 
@@ -36,6 +85,10 @@ def main():
             show_random_numbers()
         elif choice == '2':
             definitions()
+        elif choice == '3':
+            compute_payment()
+        elif choice == '4':
+            get_grade()
 
         print("\n")
         print("-----------------------------------------")
