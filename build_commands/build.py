d04830bdc3fc831cc669846027ea05fc4e82246c
Andres Asteasuain <andres.asteasuain.contractor@bydeluxe.com>
2025-09-12 11:32:57 -0300

    Mejoras al script

--- build_commands/build.py
+++ build_commands/build.py
@@ -19,7 +19,15 @@ def get_resource_path(relative_path):
 # Use the helper function to get the config path
 config_path = get_resource_path("solutions.json")
 
-def app():
+def get_environment(environment: str) -> str:
+    if environment == 'd': return 'dev'
+    elif environment == 'i': return 'int'
+    elif environment == 'u': return 'uat'
+
+def app(last_solution: str, last_tag: str) -> tuple[str, str] | None:
+
+    deploy_last_tag =  f'Last solution: {last_solution} - Last tag: {last_tag}'  if last_solution != '' and last_tag != '' else ''
+
     with open(config_path, 'r') as f:
         solutions = json.load(f)
 
@@ -29,51 +37,65 @@ def app():
     for index, solution in enumerate(solutions_sorted, 1):
         print(f' ({index}) {solution}')
 
-    solution = input('Type <solution number> [b|d] <tag> [d|i|u]\n' \
-    'For example: 1 b my-tag d\n' \
+    if deploy_last_tag != '':
+        print(f' (0) {deploy_last_tag}')
+
+    solution = input('\n .Type <solution number> [b|d] <tag> [d|i|u]\n' \
+    ' .To deploy the last build, type 0 [d|i|u]\n' \
+    '\n For example: 1 b my-tag d\n' \
     '> ')
+
     try:
         items = solution.split()
-        if len(items) < 3 or len(items) > 4:
+        if len(items) < 2 or len(items) > 4:
             print('Invalid input format')
-            quit()
+            return ('', '')
 
-        sol_index, action, tag = items[:3]
+        sol_index, action = items[:2]
+        tag = items[2] if len(items) > 2 else None
         environment = items[3] if len(items) == 4 else None
-        
+
         sol_index = int(sol_index)
-        if sol_index < 0 or sol_index > len(solutions_sorted):
-            print('Invalid solution selected')
-            quit()
-        if action not in ['b', 'd']:
-            print('Invalid action')
-            quit()
-        if action == 'd' and environment not in ['d', 'i', 'u']:
-            print('Invalid environment')
-            quit()
 
-        solution = solutions_sorted[sol_index - 1]
-
-        if action == 'b':
-            command = f'/build name={solution} tag={tag}'
+        if sol_index == 0:
+            solution = last_solution
+            tag = last_tag
+            environment = get_environment(action)
+            command = f'/deploy name={solution} tag={tag} env={environment}'
         else:
-            if environment == 'd': environment = 'dev'
-            elif environment == 'i': environment = 'int'
-            elif environment == 'u': environment = 'uat'
+            if sol_index < 0 or sol_index > len(solutions_sorted):
+                print('Invalid solution selected')
+                return ('', '')
+            if action not in ['b', 'd']:
+                print('Invalid action')
+                return ('', '')
+            if action == 'd' and environment not in ['d', 'i', 'u']:
+                print('Invalid environment')
+                return ('', '')
 
-            command = f'\n/deploy name={solution} tag={tag} env={environment}'
+            solution = solutions_sorted[sol_index - 1]
+
+            if action == 'b':
+                command = f'/build name={solution} tag={tag}'
+            else:
+                environment = get_environment(environment)
+                command = f'/deploy name={solution} tag={tag} env={environment}'
 
         pyperclip.copy(command)
         print('Command copied to clipboard:')
         print(command)
 
+        return (solution, tag)
+
     except Exception as e:
         print('Error: ', e)
 
 action = 'r'
+last_solution = ''
+last_tag = ''
 while action == 'r':
-    app()
-    action = input('Press "r" to run again, or any other key to exit: ')
+    (last_solution, last_tag) = app(last_solution, last_tag)
+    action = input('\nPress "r" to run again, or any other key to exit: ')
     action = action.lower()
    
     
